96b6f690571669938df2336b1950d72bf10025a7
Sheldon Stevens <sgste080@yahoo.com>
2021-02-20 17:39:52 -0700

    updated prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,9 @@
 // Assignment code here
-var lowerChar = "abcdefghijklmnopqrstuvwxyz";
-var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numericChar = "0123456789";
-var specialChar = "#$%&'()*+,-./:;<=>?@][^_`{|}~"
+var lowerChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numericChar = ["0","1","2","3","4","5","6","7","8","9"];
+var specialChar = ["#","$","%","&","*","+","-","<",">","?","@","^","_","~"];
+var passLength = " "
 var userPasswordChoice = [];
 
 // Get references to the #generate element
@@ -16,7 +17,7 @@ if (passLength === "" || passLength === null || isNaN(passLength)){
   window.alert("You did not enter a valid option, try again.");
   return generatePassword();
 }
-if (passlength < 8 || passLength > 128) {
+if (passLength < 8 || passLength > 128) {
   window.alert("You did not enter a valid option, try again.");
   return generatePassword();
 }
@@ -24,38 +25,46 @@ if (passlength < 8 || passLength > 128) {
 // Lowercase Prompt
 var lowerCase = window.confirm("Would you like your password to contain lowercase letters?");
 if (lowerCase) {
-  userPasswordChoice.push(lowerChar)
+  userPasswordChoice = userPasswordChoice.concat(lowerChar);
 }
 console.log(userPasswordChoice)
 
 // Uppercase Prompt
 var Uppercase = window.confirm("Would you like your password to contain UPPERCASE letters?");
 if (Uppercase) {
-  userPasswordChoice.push(upperChar)
+  userPasswordChoice = userPasswordChoice.concat(upperChar);
 }
 console.log(userPasswordChoice)
 
 // Number Prompt
 var numeric = window.confirm("Would you like your password to contain numbers?");
 if (numeric) {
-  userPasswordChoice.push(numericChar)
+  userPasswordChoice = userPasswordChoice.concat(numericChar);
 }
 console.log(userPasswordChoice)
 
 // Special Character Prompt
 var special = window.confirm("Would you like your password to contain special characters?");
 if (special) {
-  userPasswordChoice.push(specialChar)
+  userPasswordChoice = userPasswordChoice.concat(specialChar);
 }
 console.log(userPasswordChoice)
-}
 
+}
+console.log("This is the users password" + userPasswordChoice)
+var newPassword = ""
+// for loop for password choice
+for (var i = 0; i < passLength; i++) {
+  var char = Math.floor(Math.random() * userPasswordChoice)
+  newPassword += userPasswordChoice[char];
+}
+console.log(newPassword)
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.Value = password;
   passwordText.textContent = newPassword;
 
 }
